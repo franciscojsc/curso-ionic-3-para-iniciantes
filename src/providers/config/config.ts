fc9a309e30295ed0275d6aa57765803fd4c45b37
Francisco Chaves <francisco.jschaves@gmail.com>
2018-08-19 12:11:42 -0300

    Add métodos para salvar e recuperar do localStorage

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
 
+const CONFIG_KEY_NAME:string = "config";
+
 @Injectable()
 export class ConfigProvider {
 
+  private config = {
+    showSlide: false,
+    name:"",
+    username:""
+  }
+
   constructor() {
 
+  }
+
+  // Recuperar os dados do localstorage
+  getConfigData():any{
+    return localStorage.getItem(CONFIG_KEY_NAME) || {};
+  }
+
+  // Grava os dados do localstorage
+  setConfigData(showSlide?:boolean, name?:string, username?:string){
+    let config = {
+      showSlide: false,
+      name:"",
+      username:""
+    };
+
+    if(showSlide){
+      config.showSlide = showSlide;
+    }
+
+    if(name){
+      config.name = name;
+    }
+
+    if(username){
+      config.username = username;
+    }
+
+    localStorage.setItem(CONFIG_KEY_NAME, JSON.stringify(config));
   }
 
 }
